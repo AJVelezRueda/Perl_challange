2bbe3a0512a5eb7be23adcc63434b7861dac868d
AJVelezRueda <anavelezrueda@gmail.com>
2023-03-06 22:07:05 -0300

    fist step init

---
+++ step_1.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use v5.30;
+
+
+sub createRandomListOfNumbers {
+    my $listSize = $_[0];
+    my @listOfNumbers = (); 
+
+    for (my $i = 0; $i <= $listSize; $i++) {
+        push(@listOfNumbers, rand());
+    }
+
+    return @listOfNumbers;
+}
+
+sub writeAcsvFile {
+    my @listOfNumbers = @_;
+    
+    my $fileName = "output.txt";
+    open(my $FH, '>', $fileName) or die $!;
+    print $FH "@listOfNumbers \n";
+    close($FH);
+}
+
+my @randomList =  createRandomListOfNumbers(5);
+writeAcsvFile(@randomList);
