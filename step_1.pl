0ce25cf0a1bcadbf89cd81cc1d1402b80d8e5f23
AJVelezRueda <anavelezrueda@gmail.com>
2023-03-07 17:21:47 -0300

    add ramndom strings

--- step_1.pl
+++ step_1.pl
@@ -8,22 +8,37 @@ sub createRandomListOfNumbers {
     my @listOfNumbers = (); 
 
     for (my $i = 0; $i <= $listSize; $i++) {
-        push(@listOfNumbers, rand());
+        push(@listOfNumbers, int(rand(10000)));
     }
 
     return @listOfNumbers;
 }
 
+
 sub writeAcsvFile {
     my @listOfNumbers = @_;
+
     
     my $fileName = "output.txt";
     open(my $FH, '>', $fileName) or die $!;
-    print $FH "@listOfNumbers \n";
+    print $FH pop(@listOfNumbers)." , $listOfNumbers[-1] \n";
     close($FH);
+}
+
+sub createRandomSctrings {
+    my @chars = ('A'..'Z');
+    my $len = $_[0];
+    my $string = "";
+    
+    while($len--){ 
+        $string .= $chars[rand(@chars)] 
+        };
+
+    return $string;
 }
 
 if (!caller) {
     my @randomList =  createRandomListOfNumbers(5);
-    writeAcsvFile(@randomList);
+    my $randomString =  createRandomSctrings(5);
+    writeAcsvFile(@randomList, $randomString);
 }
