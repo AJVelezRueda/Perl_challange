d73881c332174211831d5383b307cababdfdc3c6
AJVelezRueda <anavelezrueda@gmail.com>
2023-03-09 13:50:38 -0300

    add multiple rows

--- step_2.pm
+++ step_2.pm
@@ -4,12 +4,19 @@ use FindBin;
 use lib $FindBin::Bin;
 use randomCsvRow;
 
-my ($number) = @ARGV;
+my ($number, $number_of_raws) = @ARGV;
 
 if (not defined $number) {
-    die "Need a number\n";
+    die "Need a the length (number) of the random strings and numbers\n";
+}
+
+if (not defined $number_of_raws) {
+    die "Need a number or rows to write\n";
 }
 
 my @randomList =  randomCsvRow::createandomListOfNumbers($number);
 my $randomString =  randomCsvRow::createRandomStrings($number);
-randomCsvRow::writeAcsvFile(@randomList, $randomString);
+
+for (my $i=0; $i <= $number_of_raws; $i++ ) {
+    randomCsvRow::writeAcsvFile(@randomList, $randomString);
+}
